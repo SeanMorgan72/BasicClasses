bd1ec647af8f9f2907bb6312221369e550027765
Sean Morgan <seanmorgan@Seans-Air.attlocal.net>
2021-08-27 15:18:33 -0500

    learning classes in c#

---
+++ BasicClasses/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace BasicClasses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car = new Car();
+            car.Make = "Chevy";
+            car.Model = "Tahoe";
+            car.Year = 2018;
+
+            Console.WriteLine(car.Make);
+            Console.WriteLine(car.Model);
+            Console.WriteLine(car.Year);
+        }
+    }
+}
